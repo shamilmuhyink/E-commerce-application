f21a077791be1ceeb4e96b490fb7e5fad3abf241
shamilmuhyin <shamilmuhyin@gmail.com>
2023-03-08 10:20:24 +0530

    Initial commit

---
+++ store/src/main/java/com/shopp/store/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.shopp.store;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable();
+        return httpSecurity.build();
+    }
+}
